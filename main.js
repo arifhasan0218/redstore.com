1b562147224cf6481598897d0457b242f84d0311
arifhasan0218 <71202653+arifhasan0218@users.noreply.github.com>
2023-05-08 17:31:25 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+  // js for toggle menu
+  var MenuItems = document.getElementById("MenuItems");
+  MenuItems.style.maxHeight = "0px"
+
+  function menutoggle(){
+      if(MenuItems.style.maxHeight == "0px")
+      {
+          MenuItems.style.maxHeight = "200px"
+      }
+      else
+          {
+              MenuItems.style.maxHeight = "0px"
+          }
+  }
+
+
+
+//   js for product gallary
+
+
+var productImg = document.getElementById("productImg");
+var smallImg = document.getElementsByClassName("smallImg");
+
+smallImg[0].onclick = function(){
+    productImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    productImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    productImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+    productImg.src = smallImg[3].src;
+}
+
+// js for toggle form
+
